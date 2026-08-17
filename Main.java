ee780f9933ae4ff85910ccae58089746a74564e9
Bhuvnesh Jangid <bhuvneshj83@gmail.com>
2026-08-17 22:44:07 +0530

    Add Java basics

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Bhuvnesh!");
+        System.out.println("Java is working perfectly!");
+    }
+}
