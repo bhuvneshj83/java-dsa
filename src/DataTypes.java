c7e4a6e56aa0efdb35ac674108065b1f2d37285c
Bhuvnesh Jangid <bhuvneshj83@gmail.com>
2026-08-28 10:26:33 +0530

    Learned Java Data Types

---
+++ src/DataTypes.java
@@ -0,0 +1,32 @@
+public class DataTypes {
+    public static void main(String[] args) {
+
+        //Numeric DT - short, byte, int, long
+
+        byte num1 = -128;
+
+        long newNum = num1;
+        System.out.println("new Num: " + newNum);
+        System.out.println(num1);
+        short num = 32767;
+        System.out.println(num);
+        long platformvisits = 122344;
+        System.out.println(platformvisits);
+        int sum = 1234567890;
+
+        float num2 = 1.23f;
+        System.out.println(sum + "\n" + num2);
+        String Value = "BHUVNESH";
+        System.out.println(Value);
+        char Work = 'b';
+        System.out.println(Work);
+        boolean eligibletovote = true;
+        System.out.println("eligibletovote");
+
+        // one more concept
+        char theycallme = 'B';
+        System.out.println("my name is: "+ (char)(theycallme + 2));
+
+    }
+}
+
