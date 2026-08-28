864fdcccd1b1a9987a53082ed81bdece23a3d90c
Bhuvnesh Jangid <bhuvneshj83@gmail.com>
2026-08-28 10:54:16 +0530

    Added Java varibles

---
+++ src/Variable.java
@@ -0,0 +1,26 @@
+public class Variable {
+    public static void main(String[] args) {
+
+        // create // declare
+        int age1;
+        age1 = 16;
+        System.out.println(age1);
+
+        // define pr initialize
+        int maxmarks = 65;
+        System.out.println(maxmarks);
+
+        //Case sensitive
+        int weight = 43;
+        int Weight = 76;
+        System.out.println("weight " + weight );
+        System.out.println("Weight " + Weight );
+
+        //Starting letter
+        int marks = 76;
+        int Marks = 67;
+        int _marks = 87;
+        int $marks = 34;
+        System.out.println(marks + " " + Marks + " " + _marks + " " + $marks);
+    }
+}
